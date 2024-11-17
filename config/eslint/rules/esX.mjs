003a03d0766d642b2bc2d3850a3f4d613f213dca
HappyUni <60145703+DFreezerR@users.noreply.github.com>
2024-11-17 04:30:21 +0300

    Update lint

--- config/eslint/rules/esX.mjs
+++ config/eslint/rules/esX.mjs
@@ -3,7 +3,7 @@ export default /** @type {import('eslint').Linter.FlatConfig} */ ({
     'es-x/no-optional-chaining': 'warn',
     'es-x/no-dynamic-import': 'off',
     'es-x/no-import-meta': 'warn',
-    'es-x/no-class-instance-fields': 'warn',
+    'es-x/no-class-instance-fields': 'off',
     'es-x/no-nullish-coalescing-operators': 'warn',
     'es-x/no-array-prototype-flat': 'warn',
   },
