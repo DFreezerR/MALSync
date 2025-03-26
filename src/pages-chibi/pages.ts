268a1cfc63e48d6d875d243400604ddc962c3ec1
HappyUni <60145703+DFreezerR@users.noreply.github.com>
2025-03-27 00:45:53 +0300

    AnimeMangaRanobeLib - pages added

--- src/pages-chibi/pages.ts
+++ src/pages-chibi/pages.ts
@@ -2,8 +2,14 @@ import { PageInterface } from './pageInterface';
 
 import { test } from './implementations/test/main';
 import { mangaNato } from './implementations/mangaNato/main';
+import { animeLib } from './implementations/animeLib/main';
+import { mangaLib } from './implementations/mangaLib/main';
+import { ranobeLib } from './implementations/ranobeLib/main';
 
 export const pages: { [key: string]: PageInterface } = {
   test,
   mangaNato,
+  animeLib,
+  mangaLib,
+  ranobeLib,
 };
--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -132,10 +132,7 @@ import { TempleScan } from './TempleScan/main';
 import { ScyllaScans } from './ScyllaScans/main';
 import { VortexScans } from './VortexScans/main';
 import { WeebCentral } from './WeebCentral/main';
-import { AnimeLib } from './AnimeLib/main';
 import { Kavita } from './Kavita/main';
-import { MangaLib } from './MangaLib/main';
-import { RanobeLib } from './RanobeLib/main';
 import { Rawkuma } from './Rawkuma/main';
 import { Aninexus } from './Aninexus/main';
 import { AnimeKAI } from './AnimeKAI/main';
@@ -276,10 +273,7 @@ export const pages = {
   ScyllaScans,
   VortexScans,
   WeebCentral,
-  AnimeLib,
   Kavita,
-  MangaLib,
-  RanobeLib,
   Rawkuma,
   Aninexus,
   AnimeKAI,
